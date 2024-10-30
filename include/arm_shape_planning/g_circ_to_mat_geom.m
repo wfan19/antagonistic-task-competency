0c15d44c0fd8c4a9e03c41a9dba915fffe00fefb
Bill Fan <fanbenlong@gmail.com>
2024-10-30 17:32:01 -0400

    Removed main project code, added index.html for website

--- include/arm_shape_planning/g_circ_to_mat_geom.m
+++
@@ -1,4 +0,0 @@
-function mat_geom = g_circ_to_mat_geom(g_circ)
-    mat_geom = [g_circ(1, :); g_circ(3, :)];
-end
-
--- include/plotting/plot_rltv_wrench_hull.m
+++
@@ -1,6 +0,0 @@
-function ax = plot_rltv_wrench_hull(rltv_bndry_af, rltv_bndry_am)
-
-
-
-end
-
